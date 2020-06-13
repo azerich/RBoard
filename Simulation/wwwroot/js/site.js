ad73cd8ff99ce18c51d3c2797183c793662c8f5f
azerich <azerich@mail.ru>
2020-06-13 23:53:13 +0300

    some trying of js validation

--- Simulation/wwwroot/js/site.js
+++ Simulation/wwwroot/js/site.js
@@ -1,83 +1,34 @@
-﻿function ClearIconClassList(object) {
-    object.classList.remove('text-danger');
-    object.classList.remove('text-success');
-    object.classList.remove('text-warning');
-}
-function SetIconClassList(object, state) {
-    ClearIconClassList(object);
-    if (state == 'set danger') {
-        object.classList.add('text-danger');
-    }
-    else if (state == 'set success') {
-        object.classList.add('text-success');
-    }
-    else {
-        //do nothing
-    }
-}
-function ClearInputClassList(object) {
+﻿function ResetInput(object) {
     object.classList.remove('is-invalid');
     object.classList.remove('is-valid');
 }
-function SetInputClassList(object, state) {
-    ClearInputClassList(object);
-    if (state == 'set invalid') {
+function SetInputState(object,state) {
+    if(state == 'invalid') {
+        ResetInput(object);
         object.classList.add('is-invalid');
     }
-    else if (state == 'set valid') {
+    else if(state == 'valid') {
+        ResetInput(object);
         object.classList.add('is-valid');
     }
     else {
-        //do nothing
-    }
-}
-function CheckUserName(object) {
-    var icon = document.getElementById('UserNameSign');
-    if (object.value.length < 5) {
-        SetInputClassList(object, 'set invalid');
-        SetIconClassList(icon, 'set danger');
-    }
-    else {
-        var link = '/Account/CheckUserName/' + object.value;
-        $.ajax(link,{
-            method: 'GET',
-            success: function (response) {
-                console.alert(response);
-            },
-            error: function () {
-                SetIconClassList(icon, 'set danger');
-                SetInputClassList(object, 'set invalid');
-                console.alert('Could not connect to server');
-            }
-        });
+        ResetInput(object);
     }
 }
 function CheckPassword(object) {
-    var icon = document.getElementById('PasswordSign');
-    var errorMessage = "";
-    var allLetters = /^[a-zA-Z]+$/;
-    var letter = /[a-zA-Z]/;
-    var number = /[0-9]/;
-
-    if (!object.value.length < 8) {
-        SetInputClassList(object, 'set invalid');
-        SetIconClassList(icon, 'set danger');
-        errorMessage += "Password must be longer than 7 characters.\n";
-    };
-    if (!allLetters.test(object)) {
-        SetInputClassList(object, 'set invalid');
-        SetIconClassList(icon, 'set danger');
-        errorMessage += "Password must contain mass letters.\n";
-    };
-    if (!letter.test(object)) {
-        SetInputClassList(object, 'set invalid');
-        SetIconClassList(icon, 'set danger');
-        errorMessage += "Password must contain letters.\n";
+    if(object.value.length < 8) {
+        SetInputState(object, 'invalid');
     }
-    if (!number.test(object)) {
-        SetInputClassList(object, 'set invalid');
-        SetIconClassList(icon, 'set danger');
-        errorMessage += "Password must contain digits.\n";
+    else {
+        SetInputState(object, 'valid');
     }
-    console.a
+}
+function CheckConfirm(object,objectId) {
+    var secondObject = document.getElementById(objectId);
+    if(object.value == secondObject.value) {
+        SetInputState(object, 'valid');
+    }
+    else {
+        SetInputState(object, 'invalid');
+    }
 }
