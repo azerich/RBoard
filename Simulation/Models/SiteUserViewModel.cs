5d2ad0b1a06734f86bd6bb2a2f00ed8144c2ff9c
Rustam ALiev <azerich@mail.ru>
2020-06-19 16:40:50 +0300

    partially implemented Account/Profile page with editing some credentials of user;

--- Simulation/Models/SiteUserViewModel.cs
+++ Simulation/Models/SiteUserViewModel.cs
@@ -6,19 +6,33 @@ namespace Simulation.Models
 {
     public class SiteUserViewModel
     {
-        [Display(Name ="Your Id on site")]
+        [Display(Name ="Id")]
+        [Required]
         public string Id { get; set; }
 
+        [Display(Name = "User name")]
+        [Required]
+        public string UserName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required]
+        public string Email { get; set; }
+
         [Display(Name = "First name")]
+        [Required]
         public string FirstName { get; set; }
 
         [Display(Name = "Middle name")]
+        [Required]
         public string MiddleName { get; set; }
 
         [Display(Name = "Last name")]
+        [Required]
         public string LastName { get; set; }
 
         [Display(Name = "Gender")]
+        [UIHint("radio")]
+        [Required]
         public GenderType? Gender { get; set; }
 
         [Display(Name ="Birth date")]
@@ -29,15 +43,12 @@ namespace Simulation.Models
         public string Locale { get; set; } = "en-us";
         
         [Display(Name = "Register date")]
-        [UIHint("date")]
-        public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
+        public string RegisterDate { get; set; } = DateTime.UtcNow.ToString();
         
         [Display(Name = "Confirm date")]
-        [UIHint("date")]
-        public DateTime? ConfirmDate { get; set; }
+        public string ConfirmDate { get; set; }
 
         [Display(Name = "Last login date")]
-        [UIHint("date")]
-        public DateTime? LastLoginDate { get; set; }
+        public string LastLoginDate { get; set; }
     }
 }
